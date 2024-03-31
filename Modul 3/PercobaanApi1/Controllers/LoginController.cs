290baa054c4049ada4fe3f82829fefde52c81e8f
ManiahMellow <jellakus@gmail.com>
2024-03-31 14:14:56 +0700

    Tugas Modul 3
    pengerjaan modul 3

---
+++ Modul 3/PercobaanApi1/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PercobaanApi1.Models;
+using System.Collections.Generic;
+
+namespace PercobaanApi1.Controllers
+{
+    public class LoginController : Controller
+    {
+        private string __constr;
+        private IConfiguration __config;
+
+        public LoginController(IConfiguration configuration)
+        {
+            __config = configuration;
+            __constr = configuration.GetConnectionString("WebApiDatabase");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("api/Login")]
+
+        public IEnumerable<Login> LoginUser(string namaUser, string password)
+        {
+            LoginContext context = new LoginContext(this.__constr);
+            List<Login> listLogin = context.Authentifikasi(namaUser, password, __config);
+            return (listLogin).ToArray();
+        }
+    }
+}
---
+++ Modul 3/PercobaanApi1/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿namespace PercobaanApi1.Models
+{
+    public class Login
+    {
+        public int id_murid { get; set; }
+        public string nama_murid { get; set; }
+        public string alamat_rumah { get; set; }
+        public string pelajaran_favorit { get; set; }
+        public int id_peran { get; set; }
+        public string nama_peran { get; set; }
+        public string token { get; set; }
+
+    }
+}
